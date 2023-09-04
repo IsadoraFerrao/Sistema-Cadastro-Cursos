b26ff43734a4939083d9cb1800e9f85ac0c614ac
Isadora <isadora-gf@hotmail.com>
2023-09-04 11:33:17 +0100

    adicionando templates

--- base/views.py
+++ base/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
 
 # Create your views here.
 def inicio(request):
     return render(request, 'inicio.html')
 
 def cadastro(request):
-    pass
+    return render(request, 'cadastro.html')
